3416e7b5e631a1f1623bd4b2871d246f29ab85dd
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2025-11-19 18:41:56 -0300

    first commit

---
+++ src/main/java/com/github/GuilhermeBauer16/EstaparTesteTecnico/EstaparTesteTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.github.GuilhermeBauer16.EstaparTesteTecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaparTesteTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaparTesteTecnicoApplication.class, args);
+	}
+
+}
